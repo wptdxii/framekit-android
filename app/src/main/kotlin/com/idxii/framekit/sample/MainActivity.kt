5a32b465b735a04e40b4494f1f9a869aece28bd6
wptdxii <wptdxii@gmail.com>
2020-07-09 16:19:59 +0800

    Init project

---
+++ app/src/main/kotlin/com/idxii/framekit/sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.idxii.framekit.sample
+
+import android.os.Bundle
+import com.idxii.framekit.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ framekit/src/main/kotlin/com/idxii/framekit/base/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.idxii.framekit.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * The base class for all activities
+ *
+ * @author wangpt
+ * @date 2020-07-09
+ */
+abstract class BaseActivity : AppCompatActivity()
